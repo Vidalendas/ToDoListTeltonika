e0bbcf5c081cc52c92c9e91baa5c696f93d3978e
Vidalendas <deividas.gudiskis@gmail.com>
2021-06-01 20:25:40 +0300

    Models
    Added users and tasks models

---
+++ Models/Task.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TODO_list.Models
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        [Required]
+        public int UserId { get; set; }
+        [Required]
+        public string TaskDescription { get; set; }
+        [Required]
+        public string State { get; set; }
+    }
+}
